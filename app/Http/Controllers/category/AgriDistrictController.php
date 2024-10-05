de31f86879fe924c57167b2cfcb739303cd19fe4
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2024-10-05 19:37:58 +0800

    Update AgriDistrictController.php

--- app/Http/Controllers/category/AgriDistrictController.php
+++ app/Http/Controllers/category/AgriDistrictController.php
@@ -73,13 +73,13 @@ class AgriDistrictController extends Controller
             $addDistrict->latitude =$request->latitude;
             $addDistrict->longitude =$request->longitude;
             // $addDistrict->altitude =$request->altitude;
-           
+            // dd($addDistrict);
             $addDistrict->save();
             return redirect('/admin-view-polygon')->with('message','AgriDistrict added successsfully');
         
         }
         catch(\Exception $ex){
-       
+            dd($ex); // Debugging statement to inspect the exception
             return redirect('/admin-add-district')->with('message','Someting went wrong');
             
         }   
@@ -158,13 +158,13 @@ public function  AgriInfoEdit($id)
             $addDistrict->latitude =$request->latitude;
             $addDistrict->longitude =$request->longitude;
             // $addDistrict->altitude =$request->altitude;
-          
+            // dd($addDistrict);
             $addDistrict->save();
             return redirect('/admin-view-polygon')->with('message','AgriDistrict updated successsfully');
         
         }
         catch(\Exception $ex){
-            // dd($ex); // Debugging statement to inspect the exception
+            dd($ex); // Debugging statement to inspect the exception
             return redirect('/admin-view-polygon')->with('message','Someting went wrong');
             
         }   
