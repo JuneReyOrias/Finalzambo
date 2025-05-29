5b441e50bd4c950e35c639182ac7fc0fd96b604c
JuneReyOrias <JuneReyOrias@users.noreply.github.com>
2025-05-29 19:19:38 +0800

    update

--- resources/views/admin/corn/farmersInfo/information.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('admin.dashb')
-@section('admin')
-
-@extends('layouts._footer-script')
-@extends('layouts._head')
-
-
-@endsection
--- resources/views/admin/crop_production/corn_crops.blade.php
+++
@@ -1,3 +0,0 @@
-@extends('admin.dashb')
-
-@section('admin')
